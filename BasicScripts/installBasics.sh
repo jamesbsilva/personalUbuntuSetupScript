ce20d85253857efbdc812333db516e7df2ec2d71
James B. Silva <jamesbsilva@email.com>
2015-02-09 02:03:14 -0500

    updates for sql

--- BasicScripts/installBasics.sh
+++ BasicScripts/installBasics.sh
@@ -14,6 +14,7 @@ echo "Install Version Control , Image Packages, IDEs, Java 7"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 sudo apt-get install synaptic gnuplot vsftpd openssh-client openssh-server vlc emacs pinta gummi imagemagick inkscape gimp eclipse eclipse-cdt eclipse-jdt git subversion meld openjdk-7-jdk scrub screen pdftk network-manager-vpnc libnet-ssleay-perl libio-socket-ssl-perl sendemail python-bs4 gksu zathura
+sudo apt-get install sqlite3
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Latex Packages"
